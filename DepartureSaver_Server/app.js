359fb374ee9653f8a9219a9badbcfb70f2942e0b
NEXON\nnnyyy <nnnyyy@nate.com>
2017-04-06 15:55:36 +0900

    소스코드 일부 수정

--- DepartureSaver_Server/app.js
+++ DepartureSaver_Server/app.js
@@ -3,11 +3,13 @@ var request = require('request');
 var xml2js = require('xml2js');
 var xmlParser = xml2js.Parser();
 var app = express();
+const dataAPIKey = '%2BldNua%2Fn0VEJt%2BtrwNRRz74Mvewgmu%2Fwz3P%2Fxtrc4GD%2BKO5Zx6oNgWYAaLpuTuBrWI6eCRMrgui%2BbdMVFvl4HQ%3D%3D';
+const weatherFreeAPIKey = '42d9865a1ddccdaa8f5d2bd8494a3f6b'
 
 app.get('/parkinginfo', function (req, res_parent) {
 
     var reqOptions = {
-        url: 'http://openapi.airport.kr/openapi/service/StatusOfParking/getTrackingParking?serviceKey=%2BldNua%2Fn0VEJt%2BtrwNRRz74Mvewgmu%2Fwz3P%2Fxtrc4GD%2BKO5Zx6oNgWYAaLpuTuBrWI6eCRMrgui%2BbdMVFvl4HQ%3D%3D&pageNo=1&startPage=1&numOfRows=10&pageSize=100',
+        url: 'http://openapi.airport.kr/openapi/service/StatusOfParking/getTrackingParking?serviceKey='+dataAPIKey+'&pageNo=1&startPage=1&numOfRows=10&pageSize=100',
         method: 'GET',
         headers: {
             'Accept' : 'application/xml',
@@ -33,7 +35,7 @@ app.get('/terminalinfo/:no', function (req, res_parent) {
 
     var no = req.params.no;
     var reqOptions = {
-        url: 'http://openapi.airport.kr/openapi/service/StatusOfDepartures/getDeparturesCongestion?serviceKey=%2BldNua%2Fn0VEJt%2BtrwNRRz74Mvewgmu%2Fwz3P%2Fxtrc4GD%2BKO5Zx6oNgWYAaLpuTuBrWI6eCRMrgui%2BbdMVFvl4HQ%3D%3D&terno=' + no,
+        url: 'http://openapi.airport.kr/openapi/service/StatusOfDepartures/getDeparturesCongestion?serviceKey='+dataAPIKey+'&terno=' + no,
         method: 'GET',
         headers: {
             'Accept' : 'application/xml',
