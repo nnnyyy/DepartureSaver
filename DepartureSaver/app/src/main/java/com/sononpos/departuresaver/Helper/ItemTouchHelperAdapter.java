b121cc505c7b2c496a9405d4a640bc603ad5feb2
DESKTOP-OIO0DRA\nnnyy <nnnyyy@nate.com>
2017-08-07 06:59:48 +0900

    공항 시간 지키미

--- DepartureSaver/app/src/main/java/com/sononpos/departuresaver/Helper/ItemTouchHelperAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.sononpos.departuresaver.Helper;
-
-/**
- * Created by nnnyy on 2017-04-14.
- */
-public interface ItemTouchHelperAdapter {
-    boolean onItemMove(int fromPosition, int toPosition);
-}
--- DepartureSaver/app/src/main/java/com/sononpos/departuresaver/MainActivity.java
+++ DepartureSaver/app/src/main/java/com/sononpos/departuresaver/MainActivity.java
@@ -2,48 +2,14 @@ package com.sononpos.departuresaver;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
 
-import com.sononpos.departuresaver.Helper.SimpleItemTouchHelperCallback;
-import com.sononpos.departuresaver.Helper.StartDragListener;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements StartDragListener {
+public class MainActivity extends AppCompatActivity {
 
     final static String TAG = "DepartureSaver";
-    private RecyclerView mRecyclerView;
-    private RecyclerView.LayoutManager mLayoutManager;
-    private MyRecyclerAdapter mAdapter;
-
-    private ItemTouchHelper mItemTouchHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        List<String> aTestList = new ArrayList<>();
-        for(int i = 0 ; i < 10 ; ++i ) {
-            aTestList.add("Test Item : " + i);
-        }
-        mAdapter = new MyRecyclerAdapter(aTestList, R.layout.my_viewholder_layout, this);
-        mRecyclerView.setAdapter(mAdapter);
-
-        //  Init ItemTouchHelper
-        SimpleItemTouchHelperCallback callback = new SimpleItemTouchHelperCallback(mAdapter);
-        mItemTouchHelper = new ItemTouchHelper(callback);
-        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
-    }
-
-    @Override
-    public void OnStartDrag(RecyclerView.ViewHolder v) {
-        mItemTouchHelper.startDrag(v);
     }
 }
